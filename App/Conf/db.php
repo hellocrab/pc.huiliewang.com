9b88155328784cd397704124dd17d9723ec15478
Lijie <591013798@qq.com>
2018-11-01 12:19:15 +0800

    Revert "对外背调以及对内背调（对外背调world导出以及详情页功能还未开发完毕）"
    This reverts commit 5d4118385fac72ccdbc027a2958fb4d581413bb7.

--- App/Conf/db.php
+++ App/Conf/db.php
@@ -1,13 +1,11 @@
 <?php
 return array(
 'DB_TYPE'=>'mysqli',
-/*'DB_HOST'=>'localhost',*/
-'DB_HOST'=>'127.0.0.1',
+'DB_HOST'=>'localhost',
 'DB_PORT'=>'3306',
 'DB_NAME'=>'pinping',
 'DB_USER'=>'root',
-/*'DB_PWD'=>'J6599cd62dea43026',*/
-'DB_PWD'=>'root',
+'DB_PWD'=>'J6599cd62dea43026',
 'DB_PREFIX'=>'mx_',
 //'connection' => 'mysql://root:J6599cd62dea43026@localhost:3306/resume'
 );
