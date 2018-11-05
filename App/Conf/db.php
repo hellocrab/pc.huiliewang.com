75de691cb727b60c63d86aed3e12d5b237fb3636
yanghao <389812249@qq.com>
2018-11-05 13:58:07 +0800

    @

--- App/Conf/db.php
+++ App/Conf/db.php
@@ -1,11 +1,23 @@
 <?php
-return array(
-'DB_TYPE'=>'mysqli',
-'DB_HOST'=>'localhost',
-'DB_PORT'=>'3306',
-'DB_NAME'=>'pinping',
-'DB_USER'=>'root',
-'DB_PWD'=>'J6599cd62dea43026',
-'DB_PREFIX'=>'mx_',
-//'connection' => 'mysql://root:J6599cd62dea43026@localhost:3306/resume'
-);
+
+if (strpos($_SERVER['SERVER_ADDR'], '192.168')) {
+    return array(
+        'DB_TYPE' => 'mysqli',
+        'DB_HOST' => 'localhost',
+        'DB_PORT' => '3306',
+        'DB_NAME' => 'pinping',
+        'DB_USER' => 'root',
+        'DB_PWD' => '123456',
+        'DB_PREFIX' => 'mx_',
+    );
+} else {
+    return array(
+    'DB_TYPE' => 'mysqli',
+    'DB_HOST' => '10.30.88.15',
+    'DB_PORT' => '3306',
+    'DB_NAME' => 'pinping',
+    'DB_USER' => 'SAP',
+    'DB_PWD' => 'bffebfb01900fe3af8a8633d3b0b7be2',
+    'DB_PREFIX' => 'mx_',
+    );
+}
