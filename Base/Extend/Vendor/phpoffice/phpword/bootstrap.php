6976f58631eb63059ceb6fdab77468e7b8e28b35
yanghao <389812249@qq.com>
2018-11-06 16:06:39 +0800

    @

--- Base/Extend/Vendor/phpoffice/phpword/bootstrap.php
+++ Base/Extend/Vendor/phpoffice/phpword/bootstrap.php
@@ -15,7 +15,7 @@
  * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  */
 
-$vendorDirPath = 'E:\github\pc.huiliewang.com\vendor';
+$vendorDirPath = __DIR__ . "/../../../vendor";
 if (file_exists($vendorDirPath . '/autoload.php')) {
     require $vendorDirPath . '/autoload.php';
 } else {
