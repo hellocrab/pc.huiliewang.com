e14d75f21d3c86e57b6859095739a878966cd4d0
yanghao <389812249@qq.com>
2018-11-06 18:25:31 +0800

    @

--- vendor/phpoffice/phpword/bootstrap.php
+++ vendor/phpoffice/phpword/bootstrap.php
@@ -15,7 +15,7 @@
  * @license     http://www.gnu.org/licenses/lgpl.txt LGPL version 3
  */
 
-$vendorDirPath = __DIR__ . '/../../vendor';
+$vendorDirPath = __DIR__ . '/../../../vendor';
 if (file_exists($vendorDirPath . '/autoload.php')) {
     require $vendorDirPath . '/autoload.php';
 } else {
