dd267fb215be25f1f44857e31866d90bcd8afad8
yanghao <389812249@qq.com>
2018-11-07 20:11:04 +0800

    @

--- App/Lib/Action/TestAction.class.php
+++ App/Lib/Action/TestAction.class.php
@@ -7,9 +7,10 @@ class TestAction extends Action{
     
     public function test()
     {
-        \ApiClient::init($appid, $secret);
+        import('@.ORG.ApiClient');
+        ApiClient::init($appid, $secret);
         $testService  = new TestServiceClient(null);
-        \ApiClient::build($testService);
+        ApiClient::build($testService);
         echo $testService->test('12345');
     }
 }
