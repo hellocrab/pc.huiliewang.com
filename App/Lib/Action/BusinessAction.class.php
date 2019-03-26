213738a3a71be9c88ae8072ca637bf6bacb0caf1
langhonglian <626724464@qq.com>
2019-03-26 16:20:55 +0800

    加入calllist后不能直接看加入的项目 加入calllist没有按照加入时间排序

--- App/Lib/Action/BusinessAction.class.php
+++ App/Lib/Action/BusinessAction.class.php
@@ -1077,14 +1077,14 @@ class BusinessAction extends Action
 
         $business_id = I("id");
         $fine_project = D("ProjectView");
-        $project['calllist'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 1, I("id"))->select();
-        $project['adviser'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 2, I("id"))->select();
-        $project['tj'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 3, I("id"))->select();
-        $project['interview'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 4, I("id"))->select();
-        $project['pass'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 5, I("id"))->select();
-        $project['offer'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 6, I("id"))->select();
-        $project['enter'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 7, I("id"))->select();
-        $project['safe'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 8, I("id"))->select();
+        $project['calllist'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 1, I("id"))->order('fine_project.addtime desc')->select();
+        $project['adviser'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 2, I("id"))->order('fine_project.updatetime desc')->select();
+        $project['tj'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 3, I("id"))->order('fine_project.tjaddtime desc')->select();
+        $project['interview'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 4, I("id"))->order('fine_project.updatetime desc')->select();
+        $project['pass'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 5, I("id"))->order('fine_project.updatetime desc')->select();
+        $project['offer'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 6, I("id"))->order('fine_project.updatetime desc')->select();
+        $project['enter'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 7, I("id"))->order('fine_project.updatetime desc')->select();
+        $project['safe'] = $fine_project->where("fine_project.status=%d and fine_project.project_id=%d", 8, I("id"))->order('fine_project.updatetime desc')->select();
 //        $project['tj'] = M("fine_project")->where("status='%s' and project_id=%d","tj",I("id"))->select();
 //        $project['interview'] = M("fine_project")->where("status='%s' and project_id=%d","interview",I("id"))->select();
 //        $project['pass'] = M("fine_project")->where("status='%s' and project_id=%d","pass",I("id"))->select();
