2199ea3832ff436ce4dcedb175aec17b36c9cac7
guoqingsong <1020074734@qq.com>
2019-04-01 16:37:27 +0800

    ###commit候选人视图

---
+++ App/Lib/Model/FineProjectCcResumeModel.class.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-04-01
+ * Time: 15:52
+ */
+
+class FineProjectCcResumeModel extends ViewModel{
+    public $viewFields;
+    public function _initialize(){
+        $this->viewFields = array(
+            'fine_project_cc'=>array('fine_id','gjtime','_type'=>'LEFT'),
+            'fine_project'=>array('resume_id','project_id','_on'=>'fine_project_cc.fine_id = fine_project.id','_type'=>'LEFT'),
+            'resume'=>array('name','_on'=>'resume.eid=fine_project.resume_id'),
+        );
+    }
+}
