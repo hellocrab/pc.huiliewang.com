aa12af021b3b7d87e8efefc1f1c4ae2701d1eb68
guoqingsong <1020074734@qq.com>
2019-04-22 18:12:03 +0800

    ###首页数据增加权限

--- App/Lib/Action/IndexAction.class.php
+++ App/Lib/Action/IndexAction.class.php
@@ -483,7 +483,7 @@ class IndexAction extends Action {
             $be[$k] = date('Y-m-d',$v);
         }
 //        M('user')->select();
-        $data = M('report_intergral')->field('sum(offer_num) as offerNum,sum(interview_num) as interviewNum,sum(fine_project_num) as fineNum,sum(integral) as integral,sum(enter_num) as enterNum')->where(array('report_date'=>array('between',$be)))->find();
+        $data = M('report_intergral')->field('sum(offer_num) as offerNum,sum(interview_num) as interviewNum,sum(fine_project_num) as fineNum,sum(integral) as integral,sum(enter_num) as enterNum')->where(array('report_date'=>array('between',$be),'user_role_id'=>array('in', getSubRoleId())))->find();
         $this->datasum = $data;
         $this->alert = parseAlert();
         $this->display();
@@ -499,23 +499,23 @@ class IndexAction extends Action {
         }
         switch ($part){
             case 'offer_num':
-                $data = M('report_intergral')->field('sum(offer_num) as offerNum')->where(array('report_date'=>array('between',$be)))->find();
+                $data = M('report_intergral')->field('sum(offer_num) as offerNum')->where(array('report_date'=>array('between',$be),'user_role_id'=>array('in', getSubRoleId())))->find();
                 $data = $data['offerNum'];
                 break;
             case 'interview_num':
-                $data = M('report_intergral')->field('sum(interview_num) as interviewNum')->where(array('report_date'=>array('between',$be)))->find();
+                $data = M('report_intergral')->field('sum(interview_num) as interviewNum')->where(array('report_date'=>array('between',$be),'user_role_id'=>array('in', getSubRoleId())))->find();
                 $data = $data['interviewNum'];
                 break;
             case 'fine_project':
-                $data = M('report_intergral')->field('sum(fine_project_num) as fineNum')->where(array('report_date'=>array('between',$be)))->find();
+                $data = M('report_intergral')->field('sum(fine_project_num) as fineNum')->where(array('report_date'=>array('between',$be),'user_role_id'=>array('in', getSubRoleId())))->find();
                 $data = $data['fineNum'];
                 break;
             case 'integral':
-                $data = M('report_intergral')->field('sum(integral) as integral')->where(array('report_date'=>array('between',$be)))->find();
+                $data = M('report_intergral')->field('sum(integral) as integral')->where(array('report_date'=>array('between',$be),'user_role_id'=>array('in', getSubRoleId())))->find();
                 $data = $data['integral'];
                 break;
             case 'enter':
-                $data = M('report_intergral')->field('sum(enter_num) as enterNum')->where(array('report_date'=>array('between',$be)))->find();
+                $data = M('report_intergral')->field('sum(enter_num) as enterNum')->where(array('report_date'=>array('between',$be),'user_role_id'=>array('in', getSubRoleId())))->find();
                 $data = $data['enterNum'];
                 break;
         }
@@ -532,7 +532,7 @@ class IndexAction extends Action {
         $type = $_GET['type'] ? BaseUtils::getStr($_GET['type']) :  '';
         switch (trim($type)){
             case 'offer' :
-                $data = $fine_project->where(array('fine_project.status'=>6))->order('fine_project.updatetime desc')->limit(10)->select();
+                $data = $fine_project->where(array('fine_project.status'=>6,'tracker'=>array('in', getSubRoleId())))->order('fine_project.updatetime desc')->limit(10)->select();
                 foreach ($data as $k=>$v){
                     $da = M('resume')->field('name,birthday,telephone,email')->where(array('eid'=>$v['resume_id']))->find();
                     $data[$k]['name'] = $da['name'];
@@ -555,7 +555,7 @@ class IndexAction extends Action {
                 }
                 break;
             case 'onjob' :
-                $data = $fine_project->where(array('fine_project.status'=>6))->order('fine_project.updatetime desc')->limit(10)->select();
+                $data = $fine_project->where(array('fine_project.status'=>6,'tracker'=>array('in', getSubRoleId())))->order('fine_project.updatetime desc')->limit(10)->select();
                 foreach ($data as $k=>$v){
                     $da = M('resume')->field('name,birthday,telephone,email')->where(array('eid'=>$v['resume_id']))->find();
                     $data[$k]['name'] = $da['name'];
@@ -578,7 +578,7 @@ class IndexAction extends Action {
                 }
                 break;
             default:
-                $data = $d_v_business->where(array('status_id'=>1))-> order('update_time desc')-> limit(10)->select();
+                $data = $d_v_business->where(array('status_id'=>1,'creator_role_id'=>array('in', getSubRoleId())))-> order('update_time desc')-> limit(10)->select();
                 include APP_PATH . "Common/city.cache.php";
                 foreach ($data as $k=>$v){
                     $data[$k]['customer'] = M('customer')->where(array('customer_id'=>$v['customer_id']))->getField('name');
