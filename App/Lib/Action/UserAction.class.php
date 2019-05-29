dd71a2ff3a121023486c49b7acb9c0cc9595bee7
langhonglian <626724464@qq.com>
2019-05-29 10:46:13 +0800

    查询调价

--- App/Lib/Action/UserAction.class.php
+++ App/Lib/Action/UserAction.class.php
@@ -2487,7 +2487,7 @@ class UserAction extends Action
         $search = BaseUtils::getStr(I('search', 0), 'string');
         $where = [];
         $departmentId > 0 && $where['parent_department_id|department_id'] = $departmentId;
-        $search && $where['phone|user_name'] = $search;
+        $search && $where['phone|user_name'] = ['like',"%{$search}%"];
         $startNo = ($page - 1) * $pageSize;
         $list = M('user_transfer')->where($where)->limit($startNo, $pageSize)->select();
         foreach ($list as &$info) {
