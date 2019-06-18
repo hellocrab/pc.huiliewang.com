217e2ffd7c33e16febb82c5bf5a730bf353d272c
guoqingsong <1020074734@qq.com>
2019-06-18 11:36:28 +0800

    ###去除推荐多余数据

--- App/Lib/Action/ProductAction.class.php
+++ App/Lib/Action/ProductAction.class.php
@@ -1516,7 +1516,7 @@ class ProductAction extends Action {
             $log_list = array_merge($log_list,$arr_adv);
         }
         //获取推荐日志
-        $arr_tj = D('ProductCView')->where(array('resume_id'=>intval($eid)))->field('com_id,tj_role_id,tjaddtime,name')->select();
+        $arr_tj = D('ProductCView')->where(array('resume_id'=>intval($eid),'tj_role_id'=>array('neq',0)))->field('com_id,tj_role_id,tjaddtime,name')->select();
         if(!empty($arr_tj)){
             $tj_roleId = array();
             foreach ($arr_tj as $k=>$v) {
